e687b37c3ed8872eedc18c937adb47510320afa2
LiliVGP <lilianrose1307@gmail.com>
2026-07-22 19:50:00 +0300

    Update extra_data.h

--- sprint3/problems/scores/solution/src/extra_data.h
+++ sprint3/problems/scores/solution/src/extra_data.h
@@ -33,10 +33,7 @@ public:
 
     const MapExtraData* GetMapExtraData(const model::Map::Id& map_id) const {
         auto it = map_data_.find(map_id);
-        if (it == map_data_.end()) {
-            return nullptr;
-        }
-        return &it->second;
+        return it != map_data_.end() ? &it->second : nullptr;
     }
 
 private:
