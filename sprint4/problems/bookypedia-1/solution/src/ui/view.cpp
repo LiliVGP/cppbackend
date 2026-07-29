c48dc0bdb836b9c127334615905f527b17c169eb
LiliVGP <lilianrose1307@gmail.com>
2026-07-29 07:30:29 +0300

    Add files via upload

--- sprint4/problems/bookypedia-1/solution/src/ui/view.cpp
+++ sprint4/problems/bookypedia-1/solution/src/ui/view.cpp
@@ -12,178 +12,174 @@ using namespace std::literals;
 namespace ph = std::placeholders;
 
 namespace ui {
-    namespace detail {
+namespace detail {
 
-        std::ostream& operator<<(std::ostream& out, const AuthorInfo& author) {
-            out << author.name;
-            return out;
+std::ostream& operator<<(std::ostream& out, const AuthorInfo& author) {
+    out << author.name;
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const BookInfo& book) {
+    out << book.title << ", " << book.publication_year;
+    return out;
+}
+
+}  // namespace detail
+
+template <typename T>
+void PrintVector(std::ostream& out, const std::vector<T>& vector) {
+    int i = 1;
+    for (auto& value : vector) {
+        out << i++ << " " << value << std::endl;
+    }
+}
+
+View::View(menu::Menu& menu, app::UseCases& use_cases, std::istream& input, std::ostream& output,
+           pqxx::connection& connection)
+    : menu_{menu}
+    , use_cases_{use_cases}
+    , input_{input}
+    , output_{output}
+    , connection_{connection} {
+    menu_.AddAction(  //
+        "AddAuthor"s, "name"s, "Adds author"s, std::bind(&View::AddAuthor, this, ph::_1)
+    );
+    menu_.AddAction("AddBook"s, "<pub year> <title>"s, "Adds book"s,
+                    std::bind(&View::AddBook, this, ph::_1));
+    menu_.AddAction("ShowAuthors"s, {}, "Show authors"s, std::bind(&View::ShowAuthors, this));
+    menu_.AddAction("ShowBooks"s, {}, "Show books"s, std::bind(&View::ShowBooks, this));
+    menu_.AddAction("ShowAuthorBooks"s, {}, "Show author books"s,
+                    std::bind(&View::ShowAuthorBooks, this));
+}
+
+bool View::AddAuthor(std::istream& cmd_input) const {
+    try {
+        std::string name;
+        std::getline(cmd_input, name);
+        boost::algorithm::trim(name);
+        if (name.empty()) {
+            throw std::runtime_error("Empty name");
         }
+        use_cases_.AddAuthor(std::move(name));
+    } catch (const std::exception&) {
+        output_ << "Failed to add author"sv << std::endl;
+    }
+    return true;
+}
 
-        std::ostream& operator<<(std::ostream& out, const BookInfo& book) {
-            out << book.title << ", " << book.publication_year;
-            return out;
+bool View::AddBook(std::istream& cmd_input) const {
+    try {
+        if (auto params = GetBookParams(cmd_input)) {
+            use_cases_.AddBook(params->author_id, params->title, params->publication_year);
         }
+    } catch (const std::exception&) {
+        output_ << "Failed to add book"sv << std::endl;
+    }
+    return true;
+}
 
-    }  // namespace detail
+bool View::ShowAuthors() const {
+    PrintVector(output_, GetAuthors());
+    return true;
+}
 
-    template <typename T>
-    void PrintVector(std::ostream& out, const std::vector<T>& vector) {
-        int i = 1;
-        for (auto& value : vector) {
-            out << i++ << " " << value << std::endl;
+bool View::ShowBooks() const {
+    PrintVector(output_, GetBooks());
+    return true;
+}
+
+bool View::ShowAuthorBooks() const {
+    try {
+        if (auto author_id = SelectAuthor()) {
+            PrintVector(output_, GetAuthorBooks(*author_id));
         }
+    } catch (const std::exception&) {
+        throw std::runtime_error("Failed to Show Books");
+    }
+    return true;
+}
+
+std::optional<detail::AddBookParams> View::GetBookParams(std::istream& cmd_input) const {
+    detail::AddBookParams params;
+
+    cmd_input >> params.publication_year;
+    std::getline(cmd_input, params.title);
+    boost::algorithm::trim(params.title);
+
+    if (params.title.empty()) {
+        return std::nullopt;
     }
 
-    View::View(menu::Menu& menu, app::UseCases& use_cases, std::istream& input, std::ostream& output,
-        pqxx::connection& connection)
-        : menu_{ menu }
-        , use_cases_{ use_cases }
-        , input_{ input }
-        , output_{ output }
-        , connection_{ connection } {
-        menu_.AddAction(  //
-            "AddAuthor"s, "name"s, "Adds author"s, std::bind(&View::AddAuthor, this, ph::_1)
-        );
-        menu_.AddAction("AddBook"s, "<pub year> <title>"s, "Adds book"s,
-            std::bind(&View::AddBook, this, ph::_1));
-        menu_.AddAction("ShowAuthors"s, {}, "Show authors"s, std::bind(&View::ShowAuthors, this));
-        menu_.AddAction("ShowBooks"s, {}, "Show books"s, std::bind(&View::ShowBooks, this));
-        menu_.AddAction("ShowAuthorBooks"s, {}, "Show author books"s,
-            std::bind(&View::ShowAuthorBooks, this));
+    auto author_id = SelectAuthor();
+    if (not author_id.has_value())
+        return std::nullopt;
+    else {
+        params.author_id = author_id.value();
+        return params;
+    }
+}
+
+std::optional<std::string> View::SelectAuthor() const {
+    output_ << "Select author:" << std::endl;
+    auto authors = GetAuthors();
+    PrintVector(output_, authors);
+    output_ << "Enter author # or empty line to cancel" << std::endl;
+
+    std::string str;
+    if (!std::getline(input_, str) || str.empty()) {
+        return std::nullopt;
     }
 
-    bool View::AddAuthor(std::istream& cmd_input) const {
-        try {
-            std::string name;
-            std::getline(cmd_input, name);
-            boost::algorithm::trim(name);
-            if (name.empty()) {
-                throw std::runtime_error("Empty name");
-            }
-            use_cases_.AddAuthor(std::move(name));
-        }
-        catch (const std::exception&) {
-            output_ << "Failed to add author"sv << std::endl;
-        }
-        return true;
+    int author_idx;
+    try {
+        author_idx = std::stoi(str);
+    } catch (std::exception const&) {
+        throw std::runtime_error("Invalid author num");
     }
 
-    bool View::AddBook(std::istream& cmd_input) const {
-        try {
-            if (auto params = GetBookParams(cmd_input)) {
-                use_cases_.AddBook(params->author_id, params->title, params->publication_year);
-            }
-        }
-        catch (const std::exception&) {
-            output_ << "Failed to add book"sv << std::endl;
-        }
-        return true;
+    --author_idx;
+    if (author_idx < 0 or author_idx >= authors.size()) {
+        throw std::runtime_error("Invalid author num");
     }
 
-    bool View::ShowAuthors() const {
-        PrintVector(output_, GetAuthors());
-        return true;
+    return authors[author_idx].id;
+}
+
+std::vector<detail::AuthorInfo> View::GetAuthors() const {
+    std::vector<detail::AuthorInfo> result;
+    pqxx::work w{connection_};
+    auto query = "SELECT id, name FROM authors ORDER BY name ASC";
+    for (auto [id, name] : w.query<std::string, std::string>(query)) {
+        result.push_back({id, name});
     }
+    return result;
+}
 
-    bool View::ShowBooks() const {
-        PrintVector(output_, GetBooks());
-        return true;
+std::vector<detail::BookInfo> View::GetBooks() const {
+    std::vector<detail::BookInfo> result;
+    pqxx::work w{connection_};
+    auto query = "SELECT title, publication_year FROM books ORDER BY title ASC";
+    for (auto [title, year] : w.query<std::string, int>(query)) {
+        result.push_back({title, year});
     }
+    return result;
+}
 
-    bool View::ShowAuthorBooks() const {
-        try {
-            if (auto author_id = SelectAuthor()) {
-                PrintVector(output_, GetAuthorBooks(*author_id));
-            }
-        }
-        catch (const std::exception&) {
-            throw std::runtime_error("Failed to Show Books");
-        }
-        return true;
+std::vector<detail::BookInfo> View::GetAuthorBooks(const std::string& author_id) const {
+    std::vector<detail::BookInfo> result;
+    pqxx::work w{connection_};
+    
+    // Исправлено: сортировка по году ASC, затем по названию ASC
+    auto query = "SELECT title, publication_year FROM books WHERE author_id = $1 ORDER BY publication_year ASC, title ASC";
+    auto res = w.exec_params(query, author_id);
+    
+    for (const auto& row : res) {
+        std::string title = row[0].as<std::string>();
+        int year = row[1].as<int>();
+        result.push_back({title, year});
     }
-
-    std::optional<detail::AddBookParams> View::GetBookParams(std::istream& cmd_input) const {
-        detail::AddBookParams params;
-
-        cmd_input >> params.publication_year;
-        std::getline(cmd_input, params.title);
-        boost::algorithm::trim(params.title);
-
-        if (params.title.empty()) {
-            return std::nullopt;
-        }
-
-        auto author_id = SelectAuthor();
-        if (not author_id.has_value())
-            return std::nullopt;
-        else {
-            params.author_id = author_id.value();
-            return params;
-        }
-    }
-
-    std::optional<std::string> View::SelectAuthor() const {
-        output_ << "Select author:" << std::endl;
-        auto authors = GetAuthors();
-        PrintVector(output_, authors);
-        output_ << "Enter author # or empty line to cancel" << std::endl;
-
-        std::string str;
-        if (!std::getline(input_, str) || str.empty()) {
-            return std::nullopt;
-        }
-
-        int author_idx;
-        try {
-            author_idx = std::stoi(str);
-        }
-        catch (std::exception const&) {
-            throw std::runtime_error("Invalid author num");
-        }
-
-        --author_idx;
-        if (author_idx < 0 or author_idx >= authors.size()) {
-            throw std::runtime_error("Invalid author num");
-        }
-
-        return authors[author_idx].id;
-    }
-
-    std::vector<detail::AuthorInfo> View::GetAuthors() const {
-        std::vector<detail::AuthorInfo> result;
-        pqxx::work w{ connection_ };
-        auto query = "SELECT id, name FROM authors ORDER BY name ASC";
-        for (auto [id, name] : w.query<std::string, std::string>(query)) {
-            result.push_back({ id, name });
-        }
-        return result;
-    }
-
-    std::vector<detail::BookInfo> View::GetBooks() const {
-        std::vector<detail::BookInfo> result;
-        pqxx::work w{ connection_ };
-        auto query = "SELECT title, publication_year FROM books ORDER BY title ASC";
-        for (auto [title, year] : w.query<std::string, int>(query)) {
-            result.push_back({ title, year });
-        }
-        return result;
-    }
-
-    std::vector<detail::BookInfo> View::GetAuthorBooks(const std::string& author_id) const {
-        std::vector<detail::BookInfo> result;
-        pqxx::work w{ connection_ };
-
-        // Используем exec_params и получаем результат через result.begin()
-        auto query = "SELECT title, publication_year FROM books WHERE author_id = $1 ORDER BY publication_year DESC, title ASC";
-        auto res = w.exec_params(query, author_id);
-
-        for (const auto& row : res) {
-            std::string title = row[0].as<std::string>();
-            int year = row[1].as<int>();
-            result.push_back({ title, year });
-        }
-
-        return result;
-    }
+    
+    return result;
+}
 
 }  // namespace ui
