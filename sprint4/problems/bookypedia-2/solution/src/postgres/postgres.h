7bcb6d4f7280ec6c38c531b8296e969118d97f58
LiliVGP <lilianrose1307@gmail.com>
2026-07-29 08:05:06 +0300

    Add files via upload

--- sprint4/problems/bookypedia-2/solution/src/postgres/postgres.h
+++ sprint4/problems/bookypedia-2/solution/src/postgres/postgres.h
@@ -1,1 +1,56 @@
+#pragma once
+#include <pqxx/connection>
+#include <pqxx/transaction>
 
+#include "../domain/author.h"
+#include "../domain/book.h"
+
+namespace postgres {
+
+    class AuthorRepositoryImpl : public domain::AuthorRepository {
+    public:
+        explicit AuthorRepositoryImpl(pqxx::connection& connection)
+            : connection_{ connection } {
+        }
+
+        void Save(const domain::Author& author) override;
+
+    private:
+        pqxx::connection& connection_;
+    };
+
+    class BookRepositoryImpl : public domain::BookRepository {
+    public:
+        explicit BookRepositoryImpl(pqxx::connection& connection)
+            : connection_{ connection } {
+        }
+
+        void Save(const domain::Book& book) override;
+
+    private:
+        pqxx::connection& connection_;
+    };
+
+    class Database {
+    public:
+        explicit Database(pqxx::connection connection);
+
+        AuthorRepositoryImpl& GetAuthors()& {
+            return authors_;
+        }
+
+        BookRepositoryImpl& GetBooks()& {
+            return books_;
+        }
+
+        pqxx::connection& GetConnection()& {
+            return connection_;
+        }
+
+    private:
+        pqxx::connection connection_;
+        AuthorRepositoryImpl authors_{ connection_ };
+        BookRepositoryImpl books_{ connection_ };
+    };
+
+}  // namespace postgres
